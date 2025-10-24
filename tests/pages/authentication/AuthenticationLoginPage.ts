b45158c8898be0d8cf40e984eac66d44de7c895e
Benoit Mirete <pro@roarth>
2025-10-24 10:34:42 +0200

    feat: enhance authentication login page and tests
    - Updated AuthenticationLoginPage to include error message handling and form submission methods.
    - Added methods for filling the login form and checking for error messages.
    - Expanded authentication tests to verify login form display and handle invalid credentials, ensuring proper error messaging is shown.

--- tests/pages/authentication/AuthenticationLoginPage.ts
+++ tests/pages/authentication/AuthenticationLoginPage.ts
@@ -1,4 +1,4 @@
-import type { Locator, Page } from '@playwright/test';
+import { expect, type Locator, type Page } from '@playwright/test';
 
 export class AuthenticationLoginPage {
   readonly page: Page;
@@ -7,21 +7,62 @@ export class AuthenticationLoginPage {
   readonly $passwordInput: Locator;
   readonly $loginButton: Locator;
 
+  readonly $errorMessage: Locator;
+
   constructor(page: Page) {
     this.page = page;
 
     this.$usernameInput = page.getByTestId('username');
     this.$passwordInput = page.getByTestId('password');
     this.$loginButton = page.getByTestId('login-button');
+
+    this.$errorMessage = page.getByTestId('error');
   }
 
   public async goto(): Promise<void> {
     await this.page.goto('/');
+    await this.isReady();
   }
 
-  public async isReady(): Promise<void> {
+  private async isReady(): Promise<void> {
     await this.$usernameInput.waitFor({ state: 'visible' });
     await this.$passwordInput.waitFor({ state: 'visible' });
     await this.$loginButton.waitFor({ state: 'visible' });
+  }
+
+  public async fillLoginForm(
+    username: string,
+    password: string
+  ): Promise<void> {
+    await this.fillUsername(username);
+    await this.fillPassword(password);
+  }
+
+  public async errorMessageIsVisible(): Promise<void> {
+    await this.$errorMessage.waitFor({ state: 'visible' });
+    const errorMessage = await this.getErrorMessage();
+    expect(errorMessage).toBe(
+      'Epic sadface: Username and password do not match any user in this service'
+    );
+  }
+
+  public async hasErrorMessage(): Promise<boolean> {
+    return await this.$errorMessage.isVisible();
+  }
+
+  private async getErrorMessage(): Promise<string | null> {
+    return await this.$errorMessage.textContent();
+  }
+
+  public async submitLoginForm(): Promise<void> {
+    await this.$loginButton.click();
+  }
+
+  private async fillUsername(username: string): Promise<void> {
+    await this.$usernameInput.fill(username);
+  }
+
+  private async fillPassword(password: string): Promise<void> {
+    await this.$passwordInput.fill(password);
   }
 }
--- tests/specs/authentication.spec.ts
+++ tests/specs/authentication.spec.ts
@@ -1,10 +1,30 @@
 import { test } from '../base';
 
-test(
-  'authentication - login form is displayed',
-  { tag: ['@ready', '@authentication', '@login'] },
-  async ({ authenticationLoginPage }) => {
-    await authenticationLoginPage.goto('/');
-    await authenticationLoginPage.isReady();
-  }
-);
+const INVALID_CREDENTIALS = {
+  username: 'invalid_username',
+  password: 'invalid_password',
+};
+
+test.describe('authentication', { tag: ['@authentication'] }, () => {
+  test(
+    'login form is displayed',
+    { tag: ['@login'] },
+    async ({ authenticationLoginPage }) => {
+      await authenticationLoginPage.goto('/');
+    }
+  );
+
+  test(
+    'login fails with invalid credentials',
+    { tag: ['@login'] },
+    async ({ authenticationLoginPage }) => {
+      await authenticationLoginPage.goto('/');
+      await authenticationLoginPage.fillLoginForm(
+        INVALID_CREDENTIALS.username,
+        INVALID_CREDENTIALS.password
+      );
+      await authenticationLoginPage.submitLoginForm();
+      await authenticationLoginPage.errorMessageIsVisible();
+    }
+  );
+});
